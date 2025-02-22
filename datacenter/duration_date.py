d9ece026ae5e5f8fa59dc959714e8d74d3334710
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-02-22 10:42:12 +0300

    I added time constants

--- datacenter/duration_date.py
+++ datacenter/duration_date.py
@@ -1,9 +1,11 @@
 from django.utils.timezone import localtime
 
 
+HOURS_CONVERTING = 3600
+MINUTES_CONVERTING = 60
 class DurationDate:
-    def is_visit_long(visit, minutes=60):
-        check_time = (visit/60 > minutes)
+    def is_visit_long(visit, minutes=MINUTES_CONVERTING):
+        check_time = (visit/MINUTES_CONVERTING > minutes)
 
         return check_time
 
@@ -15,7 +17,7 @@ class DurationDate:
         return duration.total_seconds()
 
     def format_duration(duration):
-        hours = int(duration // 3600)
-        minutes = int((duration % 3600) // 60)
+        hours = int(duration // HOURS_CONVERTING)
+        minutes = int((duration % HOURS_CONVERTING) // MINUTES_CONVERTING)
 
         return f'{hours}ч {minutes}мин'
